428366853ebc17e4e1e3be2f542e5e5c49f32528
Jared Harbison <jared.harbison@gmail.com>
2019-04-27 14:16:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // your code here
 function showRepositories(){
   console.log(this.responseText);
+  let repoLIst = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoLIst += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoLIst += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
 function getRepositories() {
   const req = new XMLHttpRequest();
